8e91cf66d2afb213b51708708dccf06d25dcb0db
quangnguyen90 <nguyenphuquang90@gmail.com>
2021-08-29 02:18:24 +0700

    4- Example about Primitive types in typescript

---
+++ src/index3.ts
@@ -0,0 +1,14 @@
+// Primitive Types
+
+const myName: string = "Quang";
+const age: number = 18;
+const bigInt: bigint = BigInt(10);
+const isFree: boolean = true;
+const someVar: undefined = undefined;
+const anotherVar: null = null;
+
+console.log(typeof myName);
+console.log(typeof age);
+console.log(typeof isFree);
+console.log(typeof someVar);
+console.log(typeof anotherVar);
