538e03d50e43ad09a099c96e92f3e8ed2aaae813
quangnguyen90 <nguyenphuquang90@gmail.com>
2021-09-05 19:06:21 +0700

    7- Optional chaining in typescript

--- src/index5.ts
+++ src/index5.ts
@@ -1,11 +1,22 @@
 // interface
 // optional properties
+// optional chaining
 import { send } from './mailer';
+
+interface Pet {
+    name: string;
+};
+
+interface Address {
+    city: string;
+};
 
 interface Contact {
     name: string;
     phone: string;
     email?: string;
+    pet?: Pet;
+    addresses?: Address[];
 };
 
 const contacts: Contact[] = [];
@@ -13,7 +24,10 @@ const contacts: Contact[] = [];
 const newContact: Contact = {
     name: 'Nguyen Phu Quang',
     phone: '0987654321',
-    email: 'abc@gmail.com'
+    email: 'abc@gmail.com',
+    pet: {
+        name: 'A',
+    },
 }
 
 const otherContact: Contact = {
@@ -27,3 +41,16 @@ contacts.push(newContact);
 if (newContact.email) {
     send(newContact.email, 'subject1', 'content1');
 }
+
+function getPetName(contact: Contact): string {
+    return contact.pet?.name || '';
+}
+
+function getFirstAddress(contact: Contact) {
+    return contact.addresses?.[0];
+}
+
+console.log(getPetName(newContact));
+console.log(getPetName(otherContact));
+
+console.log(getFirstAddress(newContact)?.city);
